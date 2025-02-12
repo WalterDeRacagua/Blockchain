f9d2520dfa9ffe0f1b2c9be2f364db6154851e1b
WalterDeRacagua <sesanc15@ucm.es>
2025-02-12 21:16:39 +0100

    Practica 1

---
+++ hello.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 < 0.8.0;
+
+contract hello {
+
+    event Print(string message);
+
+    function helloWorld() public{
+        emit Print("Hello, World!");
+    }
+}
